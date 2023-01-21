58dbcb3182f29a93dbeeef66013881b483e48fb8
shubhamshrivastavathefullstack@gmail.com <shubhamforester23@outlook.com>
2023-01-21 12:06:45 +0530

    add task

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      
+  const [ toDoList, setToDoList ] = useState([])
+  const [ newTask, setNewTask ] = useState("")
+
+  const handleChange = (event) => {
+    setNewTask(event.target.value)
+  }
+
+  const addTask = () => {
+    const newToDoList = [ ...toDoList, newTask ]
+    setToDoList(newToDoList)
+  }
+
+
+  return <div className="App">
+    <div className="addTask">
+      <input type="text" onChange={handleChange} />
+      <button onClick={addTask}>Add Task</button>
     </div>
-  );
+    <div className="list">
+      {toDoList.map((task) => {
+        return <h1>{task}</h1>
+      })}
+    </div>
+  </div>;
 }
 
 export default App;
